5bf011e21b978e4001749008538c12901a6f9015
Raja Pratap Singh <raja@enveu.com>
2021-06-09 17:48:06 +0530

    download wip

--- app/src/main/java/me/vipa/app/utils/helpers/downloads/room/DownloadEpisodeDao.java
+++ app/src/main/java/me/vipa/app/utils/helpers/downloads/room/DownloadEpisodeDao.java
@@ -18,6 +18,10 @@ public interface DownloadEpisodeDao {
     @Query("SELECT * FROM downloadedEpisodes where seriesId=:seriesId and seasonNumber=:seasonNumber")
     List<DownloadedEpisodes> getEpisodesList(@NotNull String seriesId, @NonNull String seasonNumber);
 
+    @Query("SELECT * FROM downloadedEpisodes where seriesId=:seriesId and seasonNumber=:seasonNumber and parentalRating=:parentalRating")
+    List<DownloadedEpisodes> getEpisodesListWithPG(@NotNull String seriesId, @NonNull String seasonNumber, @NonNull String parentalRating);
+
+
     @Query("SELECT * FROM downloadedEpisodes where videoId=:episodeId")
     List<DownloadedEpisodes> getEpisodes(@NotNull String episodeId);
 
